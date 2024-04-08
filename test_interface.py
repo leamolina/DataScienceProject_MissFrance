2f6093d7cc9bc753710b9465b61df6bccfe1ad5b
leamolina <120037925+leamolina@users.noreply.github.com>
2024-04-08 20:36:39 +0200

    Preprocessing

--- test_interface.py
+++ test_interface.py
@@ -8,26 +8,22 @@ import pydeck as pdk
 
 
 #Chemins
-chemin_lea = '/Users/LEAMOLINA1/Desktop/M1/S2/Projet ML : DataScience/Projet/COHEN_Anaelle_MOLINA_Lea/data_missFrance.csv'
-chemin_ana = '/Users/anaellecohen/Desktop/Cours/M1 I2D/S2/Projet:ML/Projet/DataScienceProject_MissFrance/data_missFrance.csv'
-chemin_logo_lea = '/Users/LEAMOLINA1/Desktop/M1/S2/Projet ML : DataScience/Projet/COHEN_Anaelle_MOLINA_Lea/sources/Logo_MissFrance.png'
-chemin_logo_ana = '/Users/anaellecohen/Desktop/Cours/M1 I2D/S2/Projet:ML/Projet/DataScienceProject_MissFrance/sources/Logo_MissFrance.png'
-chemin_audio_lea = "/Users/LEAMOLINA1/Desktop/M1/S2/Projet ML : DataScience/Projet/COHEN_Anaelle_MOLINA_Lea/sources/Generique_Miss_France.mp3"
-chemin_audio_ana = "/Users/anaellecohen/Desktop/Cours/M1 I2D/S2/Projet:ML/Projet/DataScienceProject_MissFrance/sources/Generique_Miss_France.mp3"
-chemin_video_lea = "/Users/LEAMOLINA1/Desktop/M1/S2/Projet ML : DataScience/Projet/COHEN_Anaelle_MOLINA_Lea/sources/Couronnement.mp4"
-chemin_video_ana = "/Users/anaellecohen/Desktop/Cours/M1 I2D/S2/Projet:ML/Projet/DataScienceProject_MissFrance/sources/Couronnement.mp4"
+chemin_database = 'data_missFrance.csv'
+chemin_logo = 'sources/Logo_MissFrance.png'
+chemin_audio = "sources/Generique_Miss_France.mp3"
+chemin_video = "sources/Couronnement.mp4"
 
 # Fonction pour afficher la page d'accueil
 def accueil():
     # Récupération des données
-    data = pd.read_csv(chemin_lea)
+    data = pd.read_csv(chemin_database)
     # data = pd.read_csv(chemin_ana)
     st.title("MISS FRANCE 2025")
-    st.image(chemin_logo_lea, use_column_width=False, output_format='auto', width=100, clamp=False, channels='RGB')
+    st.image(chemin_logo, use_column_width=False, output_format='auto', width=100, clamp=False, channels='RGB')
     st.write("Bienvenu dans notre projet  \nNous allons essayer de prédire la Miss France 2025")
 
-    st.audio(chemin_audio_lea)
-    st.video(chemin_video_lea, format="video/mp4", start_time=0)
+    st.audio(chemin_audio)
+    st.video(chemin_video, format="video/mp4", start_time=0)
 
 
 def page_un():
@@ -44,44 +40,7 @@ def page_un():
     #Tentarive de map
     france_coords = [
         (48.8566, 2.3522),  # Paris
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522), (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
-        (48.8566, 2.3522),
         (43.6045, 1.4442),  # Toulouse
-        (43.6045, 1.4442),
-        (43.6045, 1.4442),
-        (43.6045, 1.4442),
-        (43.6045, 1.4442),
-        (43.6045, 1.4442),
-        (43.6045, 1.4442),
-
         (45.7640, 4.8357),  # Lyon
         (43.2965, 5.3698),  # Marseille
         (48.1173, -1.6778),  # Rennes
@@ -118,24 +77,22 @@ def page_un():
     ))
 
 
-def page_deux():
+def prediction():
     st.title("Prédictions")
     st.write("Ici on va prédire notre future Miss France.")
 
-st.sidebar.image(chemin_logo_lea, width=100)
+st.sidebar.image(chemin_logo, width=100)
 st.sidebar.markdown(
     "<div style='text-align: center;'><h1>Navigation</h1></div>",
     unsafe_allow_html=True
 )
 page = st.sidebar.radio(
     "",
-    ("Accueil", "Présentation des données", "Prédictions")
+    ("Accueil", "Prédictions")
 )
 
 # Affichage de la page sélectionnée
 if page == "Accueil":
     accueil()
-elif page == "Présentation des données":
-    page_un()
 elif page == "Prédictions":
-    page_deux()
+    prediction()
