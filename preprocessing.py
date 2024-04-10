7490d96a91f6f96e66c1c96cc25423b554738254
leamolina <120037925+leamolina@users.noreply.github.com>
2024-04-10 19:15:53 +0200

    Début de la clase du modèle & fonction qui donne le classement en fonction de la matrice des prédicitions

--- preprocessing.py
+++ preprocessing.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
+from sklearn.linear_model import SGDClassifier
 from sklearn.preprocessing import StandardScaler, OrdinalEncoder, OneHotEncoder
 
 
@@ -46,13 +47,101 @@ while(len(columns)<88):
     columns.append("region_"+str(region))
     region += 1
 
-
-print(data_model)
+#print(data_model)
 df_new_data = pd.DataFrame.sparse.from_spmatrix(data_model, columns=columns)
-print(df_new_data)
+#print(df_new_data)
 
 
 #D'abord on ferait le preprocessing (sans pipeline ?)
 #cross validation ici (pour trouver quel est le meilleur modèle)
 #for i in range(1,12):
     #faire nos 12 modèles (tester à chaque fois quel est le meilleur modèle / meilleur hyperparamètre ou faire tout le temps le même ?)
+
+
+class MyModel(object):
+    def __init__(self, model= [SGDClassifier(loss="log", penalty="")]*12, mydata=[]):
+        self.model = model
+        self.mydata = mydata
+
+    def fit(self):
+        # Faire nos 12 modèles (tester à chaque fois quel est le meilleur modèle / meilleur hyperparamètre ou faire tout le temps le même ?)
+        list_columns_y = ["is_unranked"]+["is_"+str(i) for i in range (1,13)]
+        X = self.mydata.drop(columns=list_columns_y)
+        for i in range(1,13):
+            y=X["is_"+str(i)]
+            # Séparation train set & test set
+            self.model[i].fit(X,y)
+            #grid search
+
+
+    #Renvoyer la matrice de prédiction (celle avec toutes les probas)
+    def predict(self, X):
+        result = np.array()
+        for candidate in range(len(X)):
+            list_candidate = []
+            for i in range(1,13):
+                y_pred = self.model[i].predict_proba(X[candidate]) #Renvoie un vecteur de probabilités pour mes deux classes de chacun de mes modèles (oui ou non)
+                list_candidate.append(y_pred[0])
+            result.append(list_candidate)
+        return result
+
+
+def give_rank(initial_rank, pred_rank_matrix, list_candidate):
+    ranks = []
+
+    # Base case :
+    if len(initial_rank) >= 5 or len(initial_rank) >= len(list_candidate)+1:
+        ranks.append(initial_rank)
+        return ranks
+
+    else:
+        max_rank = np.max(pred_rank_matrix)
+        index = np.where(pred_rank_matrix == max_rank)
+        if len(index) > 0:
+            for i in range(len(index[0])):
+                personne_index = (index[0][i], index[1][i])
+
+                # La personne est seule sur sa colonne
+                nb = sum(index[1] == personne_index[1])
+                if nb == 1:
+                    new_rank = initial_rank.copy()  # Créer une copie indépendante du classement initial
+                    new_rank[list_candidate[personne_index[0]]] = personne_index[1] + 1
+                    new_rank["score"]+=max_rank
+                    new_pred_rank_matrix = pred_rank_matrix.copy()  # Créer une copie indépendante de la matrice de prédiction
+                    new_pred_rank_matrix[personne_index[0], :] = -1
+                    new_pred_rank_matrix[:, personne_index[1]] = -1
+                    ranks.extend(give_rank(new_rank, new_pred_rank_matrix, list_candidate))
+
+                # Si plusieurs personnes se trouvent sur la même colonne
+                else:
+                    for j in range(len(index[1])):
+                        if index[1][j] == personne_index[1]:
+                            new_rank = initial_rank.copy() # On crée une copie du dictionnaire qui stocke les rangs
+                            new_rank[list_candidate[index[0][j]]] = index[1][j] + 1
+                            new_rank["score"] += max_rank
+                            new_pred_rank_matrix = pred_rank_matrix.copy()  # On crée une copie de la matrice de prédiction
+                            new_pred_rank_matrix[index[0][j], :] = -1 # On met la ligne de la candidate à -1
+                            new_pred_rank_matrix[:, index[1][j]] = -1 # On met la colonne du rang à -1
+                            ranks.extend(give_rank(new_rank, new_pred_rank_matrix, list_candidate))
+
+    if not ranks:
+        ranks.append(initial_rank)
+
+    return ranks
+
+#Fontion qui donne le meilleur classement et sans répétitions
+def give_best_rank(pred_rank_matrix, list_candidate):
+    max_score = 0
+    list_dico = give_rank({'score':0}, pred_rank_matrix, list_candidate)
+    best_dico = {}
+    for dico in list_dico:
+        if dico['score'] >= max_score:
+            max_score = dico['score']
+            best_dico = dico
+    return best_dico
+
+
+classement_test= np.array([[0.1, 0.2, 0.3, 0.4],[0.3, 0.6, 0.9, 0.2],[0.9, 0.7, 0.8, 0.3],[0.4, 0.3, 0.1, 0.1],[0.8, 0.7, 0.9, 0.6]])
+list_candidate = ["Lea", "Ana", "Shirelle", "Jenna", "Shana"]
+print(give_best_rank(classement_test, list_candidate))
+
